66636eb4bd0b211559529d75f54fe2798f28eb56
Emiliano Ventura <eventura863@gmail.com>
2026-07-01 23:17:33 -0300

    Resolver conflicto entre HomeControlador y FacturaControlador

--- src/main/java/com/example/demo/controladores/HomeControlador.java
+++ src/main/java/com/example/demo/controladores/HomeControlador.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeControlador {
 
-	@GetMapping("/")
-	public String inicio() {
-		return "¡Bienvenido a GrupoEstelar! API ejecutándose correctamente en localhost:8081";
-	}
+//	@GetMapping("/")
+//	public String inicio() {
+//		return "¡Bienvenido a GrupoEstelar! API ejecutándose correctamente en localhost:8081";
+//	}
 
 	@GetMapping("/health")
 	public String health() {
